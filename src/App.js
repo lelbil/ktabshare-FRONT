558f59e949ebee8885ba518b9f8c431e88670958
lelbil <billelattouchi@yahoo.fr>
2017-11-24 01:58:30 +0100

    add basic ugly messy grid list and tiles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui
 import Drawer from 'material-ui/Drawer';
 import {GridList, GridTile} from 'material-ui/GridList';
 import BookList from './Components/BookList'
+import TextField from 'material-ui/TextField'
 
 
 class App extends Component {
@@ -13,22 +14,6 @@ class App extends Component {
     return (
       <div className="App">
         <MuiThemeProvider>
-            {/*<Toolbar id={"toolbar"}>
-                <ToolbarGroup>
-                    <ToolbarTitle text="KtabShare"/>
-                    <ToolbarSeparator/>
-                <FlatButton primary={true}>Register</FlatButton>
-                </ToolbarGroup>
-            </Toolbar>*/}
-            {/*<Drawer id={"drawer"}>
-                <h1>In Da Drawer</h1>
-            </Drawer>
-            <div id={"booksContainer"}>
-                <h1>Books need be here</h1>
-                <h3>A mind needs books same as a sword needs some stone -Some Imp</h3>
-            </div>
-            <h1>Hello World</h1>
-            <BookList/>*/}
             <div id={"boxesContainer"}>
                 <div id={"box1"} className={"box"}>
                     <Toolbar id={"toolbar"}>
@@ -40,9 +25,10 @@ class App extends Component {
                             <div id={"toolbarControls"}>
                                 <section id={"login"} className={"login"}>
                                     <form action={""} className="login">
-                                        <input type={"text"} placeholder={"Username"} />
-                                        <input type={"password"} placeholder={"Password"} />
-                                        {/*<input type={"submit"} value={"LOGIN"} />*/}
+                                        <input type={"text"} placeholder={"Username"} style={{height:"30px"}}/>
+                                        <input type={"password"} placeholder={"Password"} style={{height:"30px"}}/>
+                                        {/*<TextField hintText={"Username"} floatingLabelText={"Username"}/>
+                                        <TextField hintText={"Password"} floatingLabelText={"Password"} type={"Password"}/>*/}
                                         <FlatButton primary={true}>Login</FlatButton>
                                     </form>
                                 </section>
@@ -58,10 +44,7 @@ class App extends Component {
 
                     </div>
                     <div id={"box3"} className={"box"}>
-                        <div id={"booksContainer"}>
-                            <h1>Books need be here</h1>
-                            <h3>A mind needs books same as a sword needs some stone -Some Imp</h3>
-                        </div>
+                            <BookList/>
                     </div>
                 </div>
 
--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {GridList, GridTile} from 'material-ui/GridList'
+import './BookList.css'
 
 const data = [
     {
@@ -10,38 +11,176 @@ const data = [
 
 class BookList extends Component {
     render = () => (
-            <div>
-                <GridList cellHeight={280} cellWidth={80}>
-                    <GridTile
+                <div id={"gridList"} cols={0} cellHeight={'auto'}>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div>
+                        <GridTile className={"grid"}
+                                  key={"Key"}
+                                  title={"Title"}
+                                  subtitle={"SubTitle"}
+                        >
+                            <img src={"http://lorempixel.com/200/200/"} />
+                        </GridTile>
+                    </div>
+                    <div></div>
+                    <div></div>
+                    {/*<GridTile className={"grid"}
                         key={"Key"}
                         title={"Title"}
                         subtitle={"SubTitle"}
                     >
-                        <img src={"http://lorempixel.com/400/200/"} />
+                        <img src={"http://lorempixel.com/200/200/"} />
                     </GridTile>
-                    <GridTile
+                    <GridTile className={"grid"}
                         key={"Key"}
                         title={"Title2"}
                         subtitle={"SubTitle2"}
                     >
-                        <img src={"http://lorempixel.com/500/200/"} />
+                        <img src={"http://lorempixel.com/200/200/"} />
                     </GridTile>
-                    <GridTile
+                    <GridTile className={"grid"}
                         key={"Key"}
                         title={"titre"}
                         subtitle={"Sous-titre"}
                     >
-                        <img src={"http://lorempixel.com/500/300/"} />
+                        <img src={"http://lorempixel.com/200/200/"} />
                     </GridTile>
-                    <GridTile
+                    <GridTile className={"grid"}
                         key={"Key"}
                         title={"عنوان"}
                         subtitle={"عنوان ثاني"}
                     >
-                        <img src={"http://lorempixel.com/500/400/"} />
+                        <img src={"http://lorempixel.com/200/200/"} />
                     </GridTile>
-                </GridList>
-            </div>
+                    <GridTile className={"grid"}
+                        key={"Key"}
+                        title={"Title2"}
+                        subtitle={"SubTitle2"}
+                    >
+                        <img src={"http://lorempixel.com/200/200/"} />
+                    </GridTile>
+                    <GridTile className={"grid"}
+                        key={"Key"}
+                        title={"titre"}
+                        subtitle={"Sous-titre"}
+                    >
+                        <img src={"http://lorempixel.com/200/200/"} />
+                    </GridTile>
+                    <GridTile className={"grid"}
+                        key={"Key"}
+                        title={"عنوان"}
+                        subtitle={"عنوان ثاني"}
+                    >
+                        <img src={"http://lorempixel.com/200/200/"} />
+                    </GridTile>*/}
+                </div>
     )
 }
 
